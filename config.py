da89fc1a1236bbe868440bcf915212e20ccaabb8
kamoakon <kamoakon1@gmail.com>
2018-05-15 10:59:10 -0400

    Adding notebook and updates

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "jC5U94Ea61tjv5KOXn8GjlXJ4"
+consumer_secret = "NCred715wcpVwl0jFvgo64QgHlFyXwOf9NvVSEk35IQCoGwoxf"
+access_token = "977545708046823424-PgXlyZolYClO5CJHpzXFymVHOiK7QDe"
+access_token_secret = "ISF8fTt89WTMoBtKI5oLLul6GlDlBt4woGlrZvMAy6nWy"
